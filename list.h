07c1be6aac5bfcd71a314326a842e4d15d2287d5
jacob <jacob@plankton-pc.(none)>
2013-10-04 12:05:30 -0400

    Finally finished this code.

--- list.h
+++ list.h
@@ -1,10 +1,18 @@
 #ifndef __LIST_H__
 #define __LIST_H__
+#include <stdio.h>
+#include <stdlib.h>
 
 /* your list data structure declarations */
-
+struct node {
+	int val;
+	struct node *next;
+};
 
 /* your function declarations associated with the list */
-
+void list_clear(struct node *list);
+void printdelist(struct node *list);
+void insert_sort(const int add_val, struct node **list);
+void inserttolist(const int add_val, struct node **head);
 
 #endif // __LIST_H__
